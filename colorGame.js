5e251d6ef193c6c92e74ed9ea365ed10ba3ae64a
santiago <santiago.campos.enriquez@gmail.com>
2019-05-11 17:52:01 -0500

    Added change color of the squares when you click the correct one and pick correct square color ramndomly logic

--- colorGame.js
+++ colorGame.js
@@ -3,12 +3,13 @@ var colors = [
   "rgb(255, 255, 0)",
   "rgb(0, 255, 0)",
   "rgb(0, 255, 255)",
-  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
   "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay"); 
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +23,25 @@ for (let index = 0; index < squares.length; index++) {
     var clickedColor = this.style.backgroundColor;
     //Compare color to pickedColor
     if (clickedColor === pickedColor) {
-      alert("Correct");
+      changeColors(clickedColor);
+      messageDisplay.textContent = "Correct!";
     }else{
-      alert("Wrong");
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
 }
+
+function changeColors(color){
+  //loop through all squares
+  for(var i = 0; i < squares.length; i++){
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
