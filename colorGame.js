fe370d1c0fa2a4504b392e3609f06ad1ee448e2a
santiago <santiago.campos.enriquez@gmail.com>
2019-05-11 18:19:16 -0500

    Select pickedColor randomly and generate random colors to the squares

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay"); 
@@ -13,12 +6,12 @@ var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
-for (let index = 0; index < squares.length; index++) {
+for (var i = 0; i < squares.length; i++) {
   //Add initial colors to squares
-  squares[index].style.backgroundColor = colors[index];
+  squares[i].style.backgroundColor = colors[i];
 
   //Add click listeners to squares
-  squares[index].addEventListener("click", function(){
+  squares[i].addEventListener("click", function(){
     //grab color of clicked square
     var clickedColor = this.style.backgroundColor;
     //Compare color to pickedColor
@@ -43,5 +36,27 @@ function changeColors(color){
 function pickColor(){
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num){
+  //make an array
+  var arr = []; 
+  //Repeat num times
+  for (let i = 0; i < num; i++) {
+    //get random color and push into array
+    arr.push(randomColor());
+  }
+  //Return that array
+  return arr;
+}
+
+function randomColor() {
+  //pick a "red" from 0 - 255
+  var r = Math.floor(Math.random()*256);
+  //pick a "green" from 0 - 255
+  var g = Math.floor(Math.random()*256); 
+  //pick a "blue" from 0 - 255
+  var b = Math.floor(Math.random()*256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";  
 }
 
